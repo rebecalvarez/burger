9e5d54502b4f88da9ed090fd75d80969c8bdbe26
rebecalvarez <rebeca.dodero@gmail.com>
2019-01-20 19:44:43 -0600

    server and node update

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (name, devoured) VALUES ('Supper Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Strip Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Burger', false);
