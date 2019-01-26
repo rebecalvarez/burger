61c2ee0ac9165b0cf1cb8d6b5398d6c39edbbc43
rebecalvarez <rebeca.dodero@gmail.com>
2019-01-25 18:42:47 -0600

    adding jaws database

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,43 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+var JAWSDB_URL = "mysql://ydn581tni2s14uba:fcte0rhslllz2r2x@uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/w60y0tzv3iht5lr4";
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "uc13jynhmkss3nve.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ydn581tni2s14uba",
+    password: "fcte0rhslllz2r2x",
+    port: "3306",
+    database: "w60y0tzv3iht5lr4"
+  });
+}
+
+
+connection.connect();
 
 module.exports = connection;
+
+
+
+// var mysql = require("mysql");
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
